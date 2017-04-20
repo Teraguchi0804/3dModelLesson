bbe5fa8b05437b9741d82e5c2587272c9255ed9a
Norikazu Teraguchi <norimaki.tera@gmail.com>
2017-04-20 11:08:49 +0900

    AssimpModelLoaderのサンプルトレース中

--- src/ofApp.h
+++ src/ofApp.h
@@ -1,24 +1,37 @@
-#pragma once
+#ifndef _TEST_APP
+#define _TEST_APP
 
 #include "ofMain.h"
+#include "ofxAssimpModelLoader.h"
+#include "ofVboMesh.h"
 
 class ofApp : public ofBaseApp{
-
-	public:
-		void setup();
-		void update();
-		void draw();
-
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+    
+public:
+    void setup();
+    void update();
+    void draw();
+    
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void mouseEntered(int x, int y);
+    void mouseExited(int x, int y);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+    
+    ofxAssimpModelLoader model; //Addonの初期化
+    
+    bool bAnimate;
+    bool bAnimateMouse;
+    float animationPosition;
+    
+    ofMesh mesh;
+    ofLight	light;
 };
+
+#endif
