9f3b39eba99ef02112243112a95e199f15d8177b
Norikazu Teraguchi <norimaki.tera@gmail.com>
2017-06-29 15:40:42 +0900

    ofxEasingFunc追加

--- src/ofxEasingFunc.cpp
+++ src/ofxEasingFunc.cpp
@@ -2,17 +2,17 @@
 
 namespace ofxEasingFunc
 {
-
-const float Bounce::DH = 1. / 22.;
-const float Bounce::D1 = 1. / 11.;
-const float Bounce::D2 = 2. / 11.;
-const float Bounce::D3 = 3. / 11.;
-const float Bounce::D4 = 4. / 11.;
-const float Bounce::D5 = 5. / 11.;
-const float Bounce::D7 = 7. / 11.;
-const float Bounce::IH = 1. / DH;
-const float Bounce::I1 = 1. / D1;
-const float Bounce::I2 = 1. / D2;
-const float Bounce::I4D = 1. / D4 / D4;
-
+    
+    const float Bounce::DH = 1. / 22.;
+    const float Bounce::D1 = 1. / 11.;
+    const float Bounce::D2 = 2. / 11.;
+    const float Bounce::D3 = 3. / 11.;
+    const float Bounce::D4 = 4. / 11.;
+    const float Bounce::D5 = 5. / 11.;
+    const float Bounce::D7 = 7. / 11.;
+    const float Bounce::IH = 1. / DH;
+    const float Bounce::I1 = 1. / D1;
+    const float Bounce::I2 = 1. / D2;
+    const float Bounce::I4D = 1. / D4 / D4;
+    
 }
